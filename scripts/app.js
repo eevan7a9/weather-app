24cfe9b4aa6ee3db8499f2b5b8f7354f93353db6
eevan7a9 <e7a9@protonmail.com>
2020-01-26 12:41:45 +0900

    add togle button for users to click

--- scripts/app.js
+++ scripts/app.js
@@ -9,17 +9,21 @@ const text = document.querySelector("#text_info");
 const icon = document.querySelector("#icon");
 const calendar = document.querySelector("#calendar_ui");
 const year_month = document.querySelector("#year_month");
-const body = document.querySelector('body');
-const calendarBg = document.querySelector('.section-c');
+const body = document.querySelector("body");
+const calendarBg = document.querySelector(".section-c");
+const showMyWeather = document.querySelector("#showMyWeather");
 
-window.addEventListener("load", () => {
+showMyWeather.addEventListener("click", () => {
+  showMyWeather.textContent = "Allow Geolocation";
+  showMyWeather.disabled = true;
   setInterval(() => {
     time.innerHTML = getTime();
   }, 1000);
   getWeather(key, country, icon, description, temp, text, sunrise, sinset);
   calendarInit(calendar, year_month);
 });
-const getTextInfo = function (temperature, description, hours) {
+
+const getTextInfo = function(temperature, description, hours) {
   const temp = Math.round(temperature);
 
   if (temp >= 302) {
@@ -52,7 +56,7 @@ const getTextInfo = function (temperature, description, hours) {
     return "It's very Cold";
   }
 };
-const convertKelvin = function (value, to) {
+const convertKelvin = function(value, to) {
   if (to == "celsius") {
     return Math.round((value - 273.15) * 100) / 100 + " C&deg";
   } else if (to == "Fahrenheit") {
@@ -60,52 +64,69 @@ const convertKelvin = function (value, to) {
   }
 };
 
-const getBackground = function (description) {
-
+const getBackground = function(description) {
   switch (description) {
-    case 'clear sky':
-      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1469217329261-b173b63012f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
-      calendarBg.style.backgroundImage = "url('https://images.unsplash.com/photo-1469217329261-b173b63012f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')"
+    case "clear sky":
+      body.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1469217329261-b173b63012f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
+      calendarBg.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1469217329261-b173b63012f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
       break;
-    case 'few clouds':
-      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1565840720726-322e8aeb474b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
-      calendarBg.style.backgroundImage = "url('https://images.unsplash.com/photo-1565840720726-322e8aeb474b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
+    case "few clouds":
+      body.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1565840720726-322e8aeb474b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
+      calendarBg.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1565840720726-322e8aeb474b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
       break;
-    case 'scattered clouds':
-      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1563289723-61a8ca980c43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1439&q=80')";
-      calendarBg.style.backgroundImage = "url('https://images.unsplash.com/photo-1563289723-61a8ca980c43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1439&q=80')";
+    case "scattered clouds":
+      body.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1563289723-61a8ca980c43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1439&q=80')";
+      calendarBg.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1563289723-61a8ca980c43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1439&q=80')";
       break;
-    case 'broken clouds':
-      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1478191649591-05abc4bc29d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80')";
-      calendarBg.style.backgroundImage = "url('https://images.unsplash.com/photo-1478191649591-05abc4bc29d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80')";
+    case "broken clouds":
+      body.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1478191649591-05abc4bc29d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80')";
+      calendarBg.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1478191649591-05abc4bc29d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80')";
       break;
-    case 'shower rain':
-      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1494007485290-ce668e189d92?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')";
-      calendarBg.style.backgroundImage = "url('https://images.unsplash.com/photo-1494007485290-ce668e189d92?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')";
+    case "shower rain":
+      body.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1494007485290-ce668e189d92?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')";
+      calendarBg.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1494007485290-ce668e189d92?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')";
       break;
-    case 'rain':
-      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1498847559558-1e4b1a7f7a2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
-      calendarBg.style.backgroundImage = "url('https://images.unsplash.com/photo-1498847559558-1e4b1a7f7a2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
+    case "rain":
+      body.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1498847559558-1e4b1a7f7a2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
+      calendarBg.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1498847559558-1e4b1a7f7a2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
       break;
-    case 'thunderstorm':
-      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1574781481375-74a09eba71e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
-      calendarBg.style.backgroundImage = "url('https://images.unsplash.com/photo-1574781481375-74a09eba71e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
+    case "thunderstorm":
+      body.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1574781481375-74a09eba71e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
+      calendarBg.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1574781481375-74a09eba71e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
       break;
-    case 'snow':
-      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1547754980-3df97fed72a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
-      calendarBg.style.backgroundImage = "url('https://images.unsplash.com/photo-1547754980-3df97fed72a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
+    case "snow":
+      body.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1547754980-3df97fed72a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
+      calendarBg.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1547754980-3df97fed72a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
       break;
-    case 'mist':
-      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1482841628122-9080d44bb807?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80')";
-      calendarBg.style.backgroundImage = "url('https://images.unsplash.com/photo-1482841628122-9080d44bb807?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80')";
+    case "mist":
+      body.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1482841628122-9080d44bb807?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80')";
+      calendarBg.style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1482841628122-9080d44bb807?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80')";
       break;
 
     default:
       break;
   }
-}
+};
 
-const getTime = function (unix_time) {
+const getTime = function(unix_time) {
   let date = new Date();
   if (unix_time) {
     date = new Date(unix_time * 1000);
@@ -119,7 +140,7 @@ const getTime = function (unix_time) {
   return `${twelve_hours_format}:${minutes}:${seconds} ${am_pm}`;
 };
 
-const getWeather = function (
+const getWeather = function(
   key,
   country,
   icon,
@@ -127,7 +148,7 @@ const getWeather = function (
   temp,
   text_info,
   sunrise,
-  sunset,
+  sunset
 ) {
   let long;
   let lat;
@@ -139,21 +160,22 @@ const getWeather = function (
         // console.log(lat, long);
         fetch(
           // `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`,
-          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}2&lon=${long}&appid=${key}`,
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}2&lon=${long}&appid=${key}`
         )
           .then(response => response.json())
           .then(data => {
             // console.log(JSON.stringify(data));
             country.textContent = data.sys.country;
+            console.log(data.weather[0].icon);
             // we add icon
-            icon.src = `../img/${data.weather[0].icon}.png`;
+            icon.src = `img/${data.weather[0].icon}.png`;
             icon.classList.remove("enlarge_img");
             description.textContent = data.weather[0].description;
             temp.innerHTML = convertKelvin(data.main.temp, "celsius");
             text_info.textContent = getTextInfo(
               data.main.temp,
               data.weather[0].description,
-              new Date().getHours(),
+              new Date().getHours()
             );
             sunrise.textContent = getTime(data.sys.sunrise);
             sunset.textContent = getTime(data.sys.sunset);
@@ -164,30 +186,30 @@ const getWeather = function (
         if (error.code == error.PERMISSION_DENIED) {
           console.log("you denied me");
         }
-      },
+      }
     );
   } else {
     console.log("false");
   }
 };
-const calendarInit = function (calendar, year_month) {
+const calendarInit = function(calendar, year_month) {
   const date_full = new Date();
   const options = { month: "long" };
   const month_long = new Intl.DateTimeFormat("en-US", options).format(
-    date_full,
+    date_full
   );
   year_month.innerHTML = `${date_full.getFullYear()}, ${month_long}`;
   // we get the numbe of days this month
   const num_of_days = new Date(
     date_full.getFullYear(),
     date_full.getMonth() + 1,
-    0,
+    0
   ).getDate();
   // we get the day of the first date
   const start_day = new Date(
     date_full.getFullYear(),
     date_full.getMonth(),
-    1,
+    1
   ).getDay();
   let date_start = 1;
   // console.log(num_of_days);
